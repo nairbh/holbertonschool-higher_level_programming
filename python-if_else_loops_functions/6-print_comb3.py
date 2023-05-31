3910c47b0c87a9c1969a7cf3b3143e9910279dfd
nairbh <houcinenair@proton.me>
2023-05-31 16:11:46 +0200

    print

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-for n in range(8): 
-    for k in range(n + 1, 9): 
-        print(f"{n}{k}, ", end="") 
+for n in range(10): 
+    for k in range(n + 1, 10): 
+        if n == 8 and k == 9:
+            print(f"{n}{k}")
 
-print("89")
+        else:
+            print(f"{n}{k}, ", end="")
