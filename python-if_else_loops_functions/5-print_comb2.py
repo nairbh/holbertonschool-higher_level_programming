64a1f9b8d15b1fa53e39d34ae0ac92d4dede0f37
nairbh <houcinenair@proton.me>
2023-05-31 11:19:22 +0200

    00...99

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 
 for n in range(0, 100):
-    print("{:02d}".format(n), end=", ")
+    if n < 99:
+        print("{:02d}".format(n), end=", ")
 
-    if n < 99: 
-        pass
-    else:
-        print()
+else:
+    print("{:02d}".format(n))
