25ce92e6841e62955a83e47b8dbcefd0a611bd59
nairbh <houcinenair@proton.me>
2023-05-31 15:20:03 +0200

    print

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 
-for n in range(0, 100):
-    if n < 99:
-        print("{:02d}".format(n), end=", ")
+for n in range(0, 10):
+    for k in range(n + 1, 10):
+        print("{:02d}, ".format(n * 10 + k), end="")
 
-else:
-    print("{:02d}".format(n))
+print()
