96da3d43aee9ccea7bec66eb9eb418588ebe3e0a
nairbh <houcinenair@proton.me>
2023-06-05 22:47:57 +0200

    test

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-     for i in matrix:
-        str1 = ' '.join([str(n) for n in i])
+    for i in matrix:
+        str1 = ' '.join(str(n) for n in i)
         print(str1)
