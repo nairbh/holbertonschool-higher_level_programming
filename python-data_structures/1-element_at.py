dc72dc43454178e9cb6c574f0598bb244ada501e
nairbh <houcinenair@proton.me>
2023-06-05 10:54:52 +0200

    element

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx < len(my_list):
+    if idx < 0:
         return
-    if idx > len(my_list):
+    elif idx > len(my_list):
         return
-    for i in my_list:
-        print("{:d}{:d}".format(i))
+    else:
+
+        for i in range(len(my_list)):
+        
+            if i == idx:
     
+                return my_list[i]
