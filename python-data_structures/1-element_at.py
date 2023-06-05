0a2d9deeb912e4ad5c140f60a41fefc0f702f4aa
nairbh <houcinenair@proton.me>
2023-06-05 10:22:04 +0200

    Secure access to an element in a list

---
+++ python-data_structures/1-element_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    if idx < len(my_list):
+        return
+    if idx > len(my_list):
+        return
+    for i in my_list:
+        print("{:d}{:d}".format(i))
+    
