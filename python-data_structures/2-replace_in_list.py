d9cd64d21f5fa7c2875a085419a15a329d3d171d
nairbh <houcinenair@proton.me>
2023-06-05 13:09:50 +0200

    replace file

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -3,7 +3,7 @@ def replace_in_list(my_list, idx, element):
     if idx < 0:
         return my_list
     elif idx not in range(len(my_list)):
-        return
+        return my_list
 
     my_list[idx] = element
     return my_list
