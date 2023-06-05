bb845b1226e4e00b40e3e7551e98592c29ef1cd0
nairbh <houcinenair@proton.me>
2023-06-05 14:58:31 +0200

    new

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,6 +1,8 @@
 #!/bin/usr/python3
 def new_in_list(my_list, idx, element):
-    for n, i in enumerate(my_list):
-        if i == idx:
-            my_list[n]=element
-    return my_list
+    if idx < 0 or idx >= len(my_list):
+        return my_list.copy()
+    else:
+        new = my_list.copy()
+        new[idx] = element
+        return my_list
