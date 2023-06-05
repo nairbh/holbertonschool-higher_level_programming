be910f3f98c0b0feca794bc78a3e1c6248a145eb
nairbh <houcinenair@proton.me>
2023-06-05 13:08:03 +0200

    replace file

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -2,12 +2,8 @@
 def replace_in_list(my_list, idx, element):
     if idx < 0:
         return
-    elif idx >= len(my_list):
+    elif idx not in range(len(my_list)):
         return
-    else:
-        for n in range(len(my_list)):
-            if n == idx:
-                newlist = my_list[n]
-                my_list[n] = element
-                element = newlist
-        return my_list
+
+    my_list[idx] = element
+    return my_list
