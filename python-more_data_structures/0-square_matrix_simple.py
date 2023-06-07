aaca30ccc49f8f340a99740d452a519be5928547
nairbh <houcinenair@proton.me>
2023-06-07 10:37:48 +0200

    update

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -3,4 +3,4 @@ def square_matrix_simple(matrix=[]):
     if type(matrix) is list:
         return [square_matrix_simple(x) for x in matrix]
     else:
-        return matrix**2
+        return matrix ** 2
