840d12e64b6130ddffe9572d472f29c9ea9ac767
nairbh <houcinenair@proton.me>
2023-06-09 14:29:00 +0200

    safe

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -9,7 +9,8 @@ def safe_print_list_integers(my_list=[], x=0):
                 print("{:d}".format(my_list[count]), end="")
                 index += 1
             count += 1
-    except IndexError:
+    except ValueError:
         pass
+
     print()
     return index
