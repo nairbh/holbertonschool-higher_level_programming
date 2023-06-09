bfeabd2bdc4d36cad99f59d61f03847f0b0fec50
nairbh <houcinenair@proton.me>
2023-06-09 21:27:48 +0200

    list div

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -13,5 +13,6 @@ def list_division(my_list_1, my_list_2, list_length):
         except (IndexError):
             result = 0
             print("out of range")
-        new_list.append(result)
+        finally:
+            new_list.append(result)
     return new_list
