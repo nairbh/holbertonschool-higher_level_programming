89ff0e698e2d98794c0279b4a7d7205d8b1ca865
nairbh <houcinenair@proton.me>
2023-06-09 11:10:07 +0200

    safe

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -9,5 +9,5 @@ def safe_print_list(my_list=[], x=0):
         except ErrorValue:
             pass
         finally:
-                print()
-                return count
+            print()
+            return count
