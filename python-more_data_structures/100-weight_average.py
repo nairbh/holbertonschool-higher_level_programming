2745b9d3f684bc64ffb7af6fcee5a2ecfc44479d
nairbh <houcinenair@proton.me>
2023-06-10 15:42:23 +0200

    weight

---
+++ python-more_data_structures/100-weight_average.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+    sum_score = 0
+    sum_weight = 0
+
+    for score, weight in my_list:
+        sum_score += score * weight
+        sum_weight += weight
+    average = sum_score / sum_weight
+    return average
