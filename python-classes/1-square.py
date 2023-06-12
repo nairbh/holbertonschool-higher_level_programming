94b201505a5eef3e5207d34cd0ae165e3b72ab47
nairbh <houcinenair@proton.me>
2023-06-12 13:22:25 +0200

    square

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""python3 -c 'print(__import__("my_module").__doc__)'"""
+
+
+class Square:
+    def __init__(self, size):
+        self.__size = size
+
+
+"""python3 -c 'print(__import__("my_module").__doc__)'"""
