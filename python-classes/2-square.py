a153dd90039d5935005823deba378634a627e3b3
nairbh <houcinenair@proton.me>
2023-06-12 14:32:36 +0200

    square

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-'''python3 -c 'print(__import__("my_module").my_function.__doc__)'''
+"""Define a class"""
 
 
 class Square:
-    '''python3 -c 'print(__import__("my_module").my_function.__doc__)'''
+
+    """Define a Square"""
 
     def __init__(self, size=0):
         self.__size = size
 
-        '''python3 -c 'print(__import__("my_module").my_function.__doc__)'''
+        """Init Square with size = 0"""
 
         try:
             if not isinstance(self.__size, int):
