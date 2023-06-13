a3a158e3d82799624ff29b087d6d928d2424fbb3
nairbh <houcinenair@proton.me>
2023-06-13 10:44:44 +0200

    square

---
+++ python-classes/4-square.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""Define a class"""
+
+
+class Square:
+
+    """Define a Square"""
+
+    def __init__(self, size=0):
+
+        '''Instantiation with optional size'''
+
+        self.__size = size
+
+    def area(self):
+        '''returns the current square area'''
+        return (self.__size * self.__size)
+
+    @property
+    def size(self):
+        '''property def size'''
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        '''property setter'''
+
+        if not isinstance(value, int):
+
+            raise TypeError('size must be an integer')
+            
+        if value < 0:
+
+            raise ValueError('size must be >= 0')
+
+        self.__size = value
+
