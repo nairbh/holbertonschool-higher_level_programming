dc0eda18d494517d383cd4c916f91f5a1f267ba1
nairbh <houcinenair@proton.me>
2023-06-13 11:31:36 +0200

    square

---
+++ python-classes/5-square.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+
+'''defines a square'''
+
+class Square:
+
+    def __init__(self, size=0):
+        self.__size = size
+
+    def area(self):
+        '''returns the current square area'''
+        return self.__size * self.__size
+    
+    @property
+    def size(self):
+        '''property def size'''
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        '''setter def size'''
+        if not isinstance(value, int):
+            
+            raise TypeError('size must be an integer')
+        
+        if value < 0:
+
+            raise ValueError('size must be >= 0')
+
+        self.__size = value
+    
+    def my_print(self):
+        '''prints in stdout the square'''
+
+        if self.size == 0:
+
+            print()
+
+        for i in range(self.size):
+            for x in range(self.size):
+
+                self.value = "#"
+                print(self.value, end="")
+            print()
