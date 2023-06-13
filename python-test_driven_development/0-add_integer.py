f9283ec08bc4b0878a533238889b32750c071e64
nairbh <houcinenair@proton.me>
2023-06-13 15:22:23 +0200

    test

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
+"""define function add rwo integers"""
+
 
 def add_integer(a, b=98):
     '''function that adds 2 integers'''
-    if type(a) not in (int, float):
+    if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-    if type(b) not in (int, float):
+    if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
 
     return int(a) + int(b)
