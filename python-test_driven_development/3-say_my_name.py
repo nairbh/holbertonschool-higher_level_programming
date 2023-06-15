bfd39d4990fdba9d73ad232e8cef72953ac1084f
nairbh <houcinenair@proton.me>
2023-06-15 12:11:06 +0200

    name

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def say_my_name(first_name, last_name=""):
+    """Write a function that prints My name is <first name> <last name>"""
     if not isinstance(first_name, str):
         raise TypeError('first_name must be a string')
     if not isinstance(last_name, str):
