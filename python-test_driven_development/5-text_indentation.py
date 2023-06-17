8073f7ffd22a5c2b7c1428d69e1b3bc08c4c2de0
nairbh <houcinenair@proton.me>
2023-06-17 20:20:52 +0200

    punct

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python3
-""" print a text """
+""" function that prints a text """
 
 
 def text_indentation(text):
-    """ print a text whitout punctionation """
+    """ print texte """
+
+    punctuation = [".", "?", ":"]
+    value = False
     if not isinstance(text, str):
         raise TypeError('text must be a string')
-    punctuation = [".", "?", ":"]
-
-    new_text = ""
-    line = ""
     for i in text:
-        line += i
         if i in punctuation:
-            new_text += line.strip() + "\n\n"
-            line = ""
-
-    if line:
-        new_text += line.strip()
-
-    print(new_text)
+            print(i)
+            print()
+            value = True
+        else:
+            if value is False:
+                print(i, end="")
+            else:
+                if i != " ":
+                    print(i, end="")
+                    value = False
