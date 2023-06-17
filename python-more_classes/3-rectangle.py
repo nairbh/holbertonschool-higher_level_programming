29889a483c4a734e145c0a6c2fa3acdd358840e5
nairbh <houcinenair@proton.me>
2023-06-17 20:54:16 +0200

    rectangle

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-""" definition of a rectangle"""
+""" Real definition of a rectangle"""
 
 
 class Rectangle:
-     """ Rectangle that defines a rectangle"""
-
+    """ Rectangle that defines a rectangle"""
 
     def __init__(self, width=0, height=0):
         self.width = width
@@ -12,11 +11,11 @@ class Rectangle:
 
     @property
     def width(self):
-        return self.__width
+        return (self.__width)
 
     @width.setter
     def width(self, value):
-        if not isinstance(value, int):
+        if type(value) != int:
             raise TypeError('width must be an integer')
         if value < 0:
             raise ValueError('width must be >= 0')
@@ -24,25 +23,38 @@ class Rectangle:
 
     @property
     def height(self):
-        return self.__height
+        return (self.__height)
 
     @height.setter
     def height(self, value):
-        if not isinstance(value, int):
+        if type(value) != int:
             raise TypeError('height must be an integer')
         if value < 0:
             raise ValueError('height must be >= 0')
         self.__height = value
 
     def area(self):
-        return self.__width * self.__height
+        """  that returns the rectangle area"""
+        return (self.__width * self.__height)
 
     def perimeter(self):
+        """that returns the rectangle perimeter"""
+
         if self.__width == 0 or self.__height == 0:
             return 0
-        return 2 * (self.__width + self.__height)
+        return ((self.__width * 2) + (self.__height * 2))
 
     def __str__(self):
-        if self.__width == 0 or self.__height == 0:
-            return ""
-        return "#" * self.__width + "\n" + "#" * self.__width + "\n"
+        """that returns the rectangle with #"""
+        heightR = self.__height
+        widthR = self.__width
+        s = ""
+        if heightR == 0 or widthR == 0:
+            return s
+        else:
+            for h in range(heightR):
+                for w in range(widthR):
+                    s += "#"
+                if h != heightR - 1:
+                    s += "\n"
+            return s
