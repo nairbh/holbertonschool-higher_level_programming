13204610f813007714489bfe1f638759c1596873
nairbh <houcinenair@proton.me>
2023-06-20 00:01:03 +0200

    test

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" function that returns True if the object is exactly"""
+
+
+def is_same_class(obj, a_class):
+    """instance of the specified class"""
+
+    return type(obj) == a_class
