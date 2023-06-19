16b763b7897feb4e5fe6d3a7a6aa2a3a79a98c3a
nairbh <houcinenair@proton.me>
2023-06-19 12:58:52 +0200

    test

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
+"""Write a function that returns the list of available attributes"""
+
+
 def lookup(obj):
+    """function that returns the list"""
     ret = dir(obj)
-    if hasattr(obj,'__bases__'):
-        for base in obj.__bases__:
-            ret = ret + lookup(base)
+
+    if hasattr(obj, '___list__'):
+        for base in obj.__list__:
+            ret = ret + lookup(list)
     return ret
