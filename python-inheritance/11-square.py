bc369c996b2a3242882e389d81ff87f0c6cd99d4
nairbh <houcinenair@proton.me>
2023-06-21 00:37:27 +0200

    square

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""" square heritage"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """square"""
+    def __init__(self, size):
+        super().integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """define area """
+        return self.__size * self.__size
+
+    def __str__(self):
+        """ dunder __str__ """
+        return ("[Square] {}/{}".format(self.__size, self.__size))
