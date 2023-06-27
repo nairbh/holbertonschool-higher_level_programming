b56bbd50d02fd693fcfb9d7d3cde6aeafe92b025
nairbh <houcinenair@proton.me>
2023-06-27 19:23:23 +0200

    test

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
-""" 1-main """
+""" Module that contains class rectangle,
+inheritance of class Base
+"""
 from models.base import Base
 
 
 class Rectangle(Base):
+    """ Class rectangle """
+
     def __init__(self, width, height, x=0, y=0, id=None):
-        super().__init__(id)
+        """ Initializes setter """
         self.width = width
         self.height = height
         self.x = x
@@ -14,32 +18,56 @@ class Rectangle(Base):
 
     @property
     def width(self):
+        """Width setter"""
         return self.__width
 
     @width.setter
     def width(self, value):
+        """Width setter"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
         self.__width = value
 
     @property
     def height(self):
+        """Height setter"""
         return self.__height
 
     @height.setter
     def height(self, value):
+        """Height setter"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
         self.__height = value
 
     @property
     def x(self):
+        """X getter"""
         return self.__x
 
     @x.setter
     def x(self, value):
+        """x setter"""
+        if not isinstance(value, int):
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
         self.__x = value
 
     @property
     def y(self):
+        """y getter"""
         return self.__y
 
     @y.setter
     def y(self, value):
+        """y setter"""
+        if not isinstance(value, int):
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
         self.__y = value
