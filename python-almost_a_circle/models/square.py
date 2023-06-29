2fa315a4f2175fa95418dc2ef2377eadecc38272
nairbh <houcinenair@proton.me>
2023-06-29 12:51:08 +0200

    square dict

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -25,17 +25,20 @@ class Square(Rectangle):
         self.height = value
 
     def update(self, *args, **kwargs):
-        """ update square"""
-        for key, value, in kwargs.items():
+        """def update attribut"""
+        for key, value in kwargs.items():
             setattr(self, key, value)
+
         if len(args) >= 1:
             self.id = args[0]
         if len(args) >= 2:
-            self.size = args[1]
+            self.width = args[1]
         if len(args) >= 3:
-            self.x = args[2]
+            self.height = args[2]
         if len(args) >= 4:
-            self.y = args[3]
+            self.x = args[3]
+        if len(args) >= 5:
+            self.y = args[4]
 
     def to_dictionary(self):
         """dictionary"""
