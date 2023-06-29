514fbe2767c7b085c6ba81a2a7d30a2d071876ac
nairbh <houcinenair@proton.me>
2023-06-29 11:51:53 +0200

    squre

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""write a class"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """def class"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)
+        self.size = size
+
+    def __str__(self):
+        """Square"""
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """size"""
+        self.width = value
+        self.height = value
