25adf82005c82a4133e4790e72b5643d83372b32
nairbh <houcinenair@proton.me>
2023-06-29 14:27:00 +0200

    base

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -24,3 +24,14 @@ class Base:
         else:
             json_format = json.dumps(list_dictionaries)
             return json_format
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """ save file """
+        filename = cls.__name__ + ".json"
+        list_of_obj = []
+        if list_objs is not None:
+            for obj in list_objs:
+                list_of_obj.append(obj.to_dictionary())
+        with open(filename, "w", encoding="UTF-8") as file:
+            file.write(cls.to_json_string(list_of_obj))
