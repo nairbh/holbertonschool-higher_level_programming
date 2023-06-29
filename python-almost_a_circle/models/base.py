f59dd92b12ba7cb42d8ed8d1da53aa6821afb8ac
nairbh <houcinenair@proton.me>
2023-06-29 13:12:50 +0200

    base

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -13,7 +13,8 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
-
+    
+    @staticmethod
     def to_json_string(list_dictionaries):
         """ def list of dict """
         if list_dictionaries is None or len(list_dictionaries) == 0:
