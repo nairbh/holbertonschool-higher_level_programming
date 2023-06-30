af68ca6c6ac52e2727bccdf63240ebaefb4f4113
Houcine <113390396+nairbh@users.noreply.github.com>
2023-06-30 14:11:29 +0200

    Update rectangle.py

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -84,7 +84,7 @@ class Rectangle(Base):
     def __str__(self):
         """ str def """
         message = (
-            f"[Rectangle] ({self.id}) "
+            f"[Rectangle] ({self.id})"
             f"{self.x}/{self.y} - {self.width}/{self.height}").strip()
         return message
 
@@ -100,15 +100,15 @@ class Rectangle(Base):
         for key, value in kwargs.items():
             setattr(self, key, value)
 
-        if len(args) >= 1:
+        if len(args) >= 0:
             self.id = args[0]
-        if len(args) >= 2:
+        if len(args) >= 1:
             self.width = args[1]
-        if len(args) >= 3:
+        if len(args) >= 2:
             self.height = args[2]
-        if len(args) >= 4:
+        if len(args) >= 3:
             self.x = args[3]
-        if len(args) >= 5:
+        if len(args) >= 4:
             self.y = args[4]
 
     def to_dictionary(self):
