1ed56257f3218585e61221a2e9c2fd0ff2f59c48
nairbh <houcinenair@proton.me>
2023-06-30 18:17:45 +0200

    test

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -90,15 +90,15 @@ class Rectangle(Base):
     def update(self, *args, **kwargs):
         """ Update the attributes of the rectangle """
         if args:
-            if len(args) >= 0:
-                self.id = args[0]
             if len(args) >= 1:
-                self.width = args[1]
+                self.id = args[0]
             if len(args) >= 2:
-                self.height = args[2]
+                self.width = args[1]
             if len(args) >= 3:
-                self.x = args[3]
+                self.height = args[2]
             if len(args) >= 4:
+                self.x = args[3]
+            if len(args) >= 5:
                 self.y = args[4]
         else:
             for key, value in kwargs.items():
--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -28,13 +28,13 @@ class Square(Rectangle):
         """def update attribut"""
         if args:
 
-            if len(args) >= 0:
-                self.id = args[0]
             if len(args) >= 1:
-                self.size = args[1]
+                self.id = args[0]
             if len(args) >= 2:
-                self.x = args[2]
+                self.size = args[1]
             if len(args) >= 3:
+                self.x = args[2]
+            if len(args) >= 4:
                 self.y = args[3]
         else:
             for key, value in kwargs.items():
