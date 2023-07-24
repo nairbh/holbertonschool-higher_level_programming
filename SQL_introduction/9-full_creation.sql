e76a2db64984173dc4e65dd6f9e2fa45e205eb10
nairbh <houcinenair@proton.me>
2023-07-24 14:31:55 +0200

    update order of table value

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,12 +1,12 @@
 --  creates a table second_table in the database hbtn_0c_0 in your MySQL server and add multiples rows.
 CREATE TABLE IF NOT EXISTS second_table(
 	id INT,
-	score INT,
-	name VARCHAR(256)
+	name VARCHAR(256),
+	score INT
 	);
 INSERT INTO second_table(id, score, name)
 VALUES
-	(1, 10, 'Jhone'),
-	(2, 3, 'Alex'),
-	(3, 14, 'Bob'),
-	(4, 8, 'George');
+	(1, 'Jhone', 10),
+	(2, 'Alex', 3),
+	(3, 'Bob', 14),
+	(4, 'George', 8);
