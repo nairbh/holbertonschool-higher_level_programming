2b5712af560a484fd25fe952cb42e070c5ffdc44
nairbh <houcinenair@proton.me>
2023-07-24 15:27:57 +0200

    select up than 10 scored

---
+++ SQL_introduction/11-best_score.sql
@@ -0,0 +1,3 @@
+--  that lists all records with a score >= 10 in the table second_table of the database hbtn_0c_0 in your MySQL server.
+select distinct score, name from second_table
+where score >=10;
