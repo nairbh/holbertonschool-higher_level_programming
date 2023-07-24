4dd39966d332856f95b6c90f228e9288021cfdea
nairbh <houcinenair@proton.me>
2023-07-24 16:09:26 +0200

    update code

--- SQL_introduction/12-no_cheating.sql
+++ SQL_introduction/12-no_cheating.sql
@@ -1,3 +1,3 @@
 -- updates the score of Bob to 10 in the table second_table.
-UPDATE second_table SET score = REPLACE(score, 14, '10')
+UPDATE second_table SET score = REPLACE(score, 14, 10)
 WHERE name = 'Bob'
