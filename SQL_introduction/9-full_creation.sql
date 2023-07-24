eaa9649ec0483cee10d5b43d5e70e4cf30b13fea
nairbh <houcinenair@proton.me>
2023-07-24 13:57:05 +0200

    create full database

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,12 @@
+--  creates a table second_table in the database hbtn_0c_0 in your MySQL server and add multiples rows.
+CREATE TABLE IF NOT EXISTS second_table(
+	id INT,
+	score INT,
+	name VARCHAR(256) NOT NULL
+	);
+INSERT INTO second_table(id, score, name)
+VALUES
+	(1, 10, 'Jhone'),
+	(2, 3, 'Alex'),
+	(3, 14, 'Bob'),
+	(4, 8, 'George');
