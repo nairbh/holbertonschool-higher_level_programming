5ba2e243ee8edf2ec953658ffad2d83c31b0115f
nairbh <houcinenair@proton.me>
2023-07-25 19:59:40 +0200

    requete script

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,19 @@
 -- Write a script that creates the database hbtn_0d_2 and the user user_0d_2.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE USER IF NOT EXISTS user_0d_2 @localhost IDENTIFIED BY user_0d_2_pwd;
-GRANT SELECT ON hbtn_0d_2.* TO user_0d_2 @localhost;
+USE hbtn_0d_2;
+
+
+CREATE PROCEDURE create_user_if_not_exists()
+BEGIN
+    DECLARE user_exists INT DEFAULT 0;
+
+    SELECT COUNT(*) INTO user_exists FROM mysql.user WHERE user = 'user_0d_2' AND host = 'localhost';
+
+    IF user_exists = 0 THEN
+        CREATE USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+        GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+    END IF;
+END 
+CALL create_user_if_not_exists();
+DROP PROCEDURE IF EXISTS create_user_if_not_exists;
 FLUSH PRIVILEGES;
