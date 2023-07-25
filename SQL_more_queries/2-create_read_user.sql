8c39f2a45dffae55ccf37febc455f699ea998f66
nairbh <houcinenair@proton.me>
2023-07-25 20:15:14 +0200

    requete

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,3 @@
-
 -- Write a script that creates the database hbtn_0d_2 and the user user_0d_2.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 USE hbtn_0d_2;
@@ -11,7 +10,7 @@ BEGIN
     SELECT COUNT(*) INTO user_exists FROM mysql.user WHERE user = 'user_0d_2' AND host = 'localhost';
 
     IF user_exists = 0 THEN
-        SET @create_user_query = CONCAT('CREATE USER ', 'user_0d_2', '@localhost IDENTIFIED BY ', 'user_0d_2_pwd', ';');
+        SET @create_user_query = CONCAT('CREATE USER ''user_0d_2''@''localhost'' IDENTIFIED BY ''user_0d_2_pwd'';');
         PREPARE stmt FROM @create_user_query;
         EXECUTE stmt;
         DEALLOCATE PREPARE stmt;
