9f34e628e5fc217ba3a34e352bbca4cb5ff4060d
nairbh <houcinenair@proton.me>
2023-07-25 17:51:37 +0200

    update

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -1,2 +1,2 @@
 -- Write a script that creates the table force_name on your MySQL server.
-CREATE TABLE IF NOT EXISTS unique_id(id INT UNIQUE, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS 'unique_id' ('id' INT UNIQUE, 'name' VARCHAR(256));
