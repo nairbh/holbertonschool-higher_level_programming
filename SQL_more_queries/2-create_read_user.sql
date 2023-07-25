99fb8133efedfde6b7f16354cbced495cd871d70
nairbh <houcinenair@proton.me>
2023-07-25 21:15:44 +0200

    test

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,24 +1,6 @@
 -- Write a script that creates the database hbtn_0d_2 and the user user_0d_2.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-USE hbtn_0d_2;
 
-DELIMITER $$
-DROP PROCEDURE IF EXISTS create_user_if_not_exists;
-CREATE PROCEDURE create_user_if_not_exists()
-BEGIN
-    DECLARE user_exists INT DEFAULT 0;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 
-    SELECT COUNT(*) INTO user_exists FROM mysql.user WHERE user = 'user_0d_2' AND host = 'localhost';
-
-    IF user_exists = 0 THEN
-        SET @create_user_query = CONCAT('CREATE USER ''user_0d_2''@''localhost'' IDENTIFIED BY ''user_0d_2_pwd''');
-        PREPARE stmt FROM @create_user_query;
-        EXECUTE stmt;
-        DEALLOCATE PREPARE stmt;
-
-        GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
-    END IF;
-END $$
-DELIMITER ;
-
-CALL create_user_if_not_exists();
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
