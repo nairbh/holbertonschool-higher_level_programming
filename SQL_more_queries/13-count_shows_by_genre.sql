f322c78167bd770674bd8ccca450b906e1d4b70b
nairbh <houcinenair@proton.me>
2023-07-27 15:37:43 +0200

    groupe

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,4 +1,6 @@
--- Import the database dump from hbtn_0aaaaad_tvshows to your MySQL server: download (same as 11-genre_id_all_shows.sql
-
-SET sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));
-SELECT tv_genres.name, count(*) AS number_of_shows FROM tv_genres JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id GROUP BY tv_genres.name ORDER BY tv_genres.id DESC;
+-- List all genre from tv_shows with number
+SELECT tv_genres.name AS genre, COUNT(*) AS number_of_shows
+FROM tv_genres
+JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY tv_genres.name
+ORDER BY number_of_shows DESC;
