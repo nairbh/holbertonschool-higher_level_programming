4a9e0594c406ad4baf359917cd52450c2ea4f403
nairbh <houcinenair@proton.me>
2023-07-31 13:45:38 +0200

    test

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
-"""Write a script that lists all states from the database hbtn_0e_0_usa"""
-import sys
-import MySQLdb
+""" Write a script that lists all states from the database hbtn_0e_0_usa: """
+if __name__ == '__main__':
+    import sys
+    import MySQLdb
 
-
-if __name__ == "__main__":
     if len(sys.argv) != 4:
-        sys.exit("Use: file_name, username, passeword, db ")
-    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], password=sys.argv[2], database=sys.argv[3], charset="utf8")    
-    with conn.cursor() as cur:
-        cur.execute("SELECT * FROM states ORDER BY id ASC")
-        for row in cur.fetchall():
-            print(row)
+        sys.exit('Use: 0-select_states.py <mysql username> <mysql password>'
+                 ' <database name>')
+
+    conn = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3], charset='utf8')
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
     cur.close()
     conn.close()
