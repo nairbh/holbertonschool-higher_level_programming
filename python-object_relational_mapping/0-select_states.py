369a43beac896b844a1dc33f63106a842cde7378
nairbh <houcinenair@proton.me>
2023-07-31 11:40:47 +0200

    root

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Write a script that lists all states from the database hbtn_0e_0_usa"""
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        sys.exit("Use: file_name, username, passeword, db ")
+    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], password=sys.argv[2], database=sys.argv[3], charset="utf8")    
+    with conn.cursor() as cur:
+        cur.execute("SELECT * FROM states ORDER BY id ASC")
+        for row in cur.fetchall():
+            print(row)
+    cur.close()
+    conn.close()
