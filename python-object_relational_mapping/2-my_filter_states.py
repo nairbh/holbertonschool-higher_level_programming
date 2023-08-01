40775e27b55cc6eda9bac706b2e297a6050ea09e
nairbh <houcinenair@proton.me>
2023-08-01 03:35:50 +0200

    arizona

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,25 +1,18 @@
 #!/usr/bin/python3
-""" write a script that lists all states from the database hbtn_0e_0_usa """
-import sys
+""" script that takes in an argument and displays all values in the states
+table of hbtn_0e_0_usa where name matches the argument """
+
 import MySQLdb
+import sys
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 5:
-        sys.exit("Usage: file_name, username, password, db, state_name")
-
-    username, password, db_name, state_name = sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]
-
-    conn = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=username,
-        passwd=password,
-        db=db_name
-    )
-
+    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3])
     with conn.cursor() as cur:
-        cur.execute("SELECT * FROM states WHERE name = %s ORDER BY id", (state_name,))
+        cur.execute("SELECT * FROM states WHERE BINARY name = '{}'\
+                ORDER BY id ASC".format(sys.argv[4]))
         for row in cur.fetchall():
             print(row)
+
     conn.close()
