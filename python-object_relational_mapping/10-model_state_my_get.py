1afdee494be5e390bd2861cf704ae641d70f19d6
nairbh <houcinenair@proton.me>
2023-08-01 07:43:14 +0200

    print object argument

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -7,15 +7,18 @@ from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
 if __name__ == "__main__":
-    username, password, db, state_name = sys.argv[1],
-    sys.argv[2], sys.argv[3], sys.argv[4]
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db = sys.argv[3]
+    state_name = sys.argv[4]
+
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
                            .format(username, password, db))
     Session = sessionmaker(bind=engine)
     session_encap = Session()
 
     state = session_encap.query(State).filter_by(name=state_name).first()
-    print("{}: {}".format(state.id, state_name)
-          if state is not None else "Not found")
+    print("{}: {}".format(state.id, state_name) if state
+          is not None else "Not found")
 
     session_encap.close()
