67476ff81547b057c272d523daa43eb3efd5f9e4
nairbh <houcinenair@proton.me>
2023-08-01 05:10:03 +0200

    model

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python3
-""" Class definition of a State and an instance Base = declarative_base() """
+"""class states
+"""
+
 
 from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy import Sequence
 from sqlalchemy.ext.declarative import declarative_base
+
 
 Base = declarative_base()
 
 
 class State(Base):
-
+    """states class"""
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    id = Column(Integer, primary_key=True,
+                nullable=False, autoincrement=True, unique=True)
     name = Column(String(128), nullable=False)
