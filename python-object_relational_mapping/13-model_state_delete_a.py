084b7f29fe268fc1dcc59a84ca6e7b6c9bf2a570
nairbh <houcinenair@proton.me>
2023-08-03 01:24:25 +0200

    rw

--- python-object_relational_mapping/13-model_state_delete_a.py
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,22 +1,24 @@
 #!/usr/bin/python3
-"""script that deletes all State objects with a name containing the letter
-a from the database hbtn_0e_6_usa"""
+"""States class"""
 
-import sys
 from model_state import Base, State
+import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+
 
 if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
-                           .format(username, password, database),
-                           pool_pre_ping=True)
+
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".
+                           format(username, password, database))
+
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    for row in session.query(State).filter(State.name.contains('a')):
-        session.delete(row)
+    States = session.query(State).filter(State.name.like("%a%")).all()
+    for state in States:
+        session.delete(state)
     session.commit()
-    session.close()
--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-"""script 14-model_city_fetch_by_state.py that prints all City objects
-from the database hbtn_0e_14_usa"""
+"""Prints all City objects from the database hbtn_0e_14_usa"""
 
-import sys
+from model_city import Base, City
 from model_state import Base, State
-from model_city import City
+import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
@@ -12,13 +11,15 @@ if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
-                           .format(username, password, database),
-                           pool_pre_ping=True)
+
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".
+                           format(username, password, database))
+
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    for s, c in session.query(State, City).\
-            filter(State.id == City.state_id).\
-            order_by(City.id):
-        print("{}: ({}) {}".format(s.name, c.id, c.name))
+    States = session.query(State, City).filter(State.id ==
+                                               City.state_id).order_by(City.id)
+    for state, cities in States:
+        print("{}: ({}) {}".format(state.name, cities.id, cities.name))
     session.close()
