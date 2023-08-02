0370f01ab2c9f3e8c75fd7b7fd5b5cb5e052997f
nairbh <houcinenair@proton.me>
2023-08-03 01:14:29 +0200

    model

---
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""script 14-model_city_fetch_by_state.py that prints all City objects
+from the database hbtn_0e_14_usa"""
+
+import sys
+from model_state import Base, State
+from model_city import City
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
+                           .format(username, password, database),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for s, c in session.query(State, City).\
+            filter(State.id == City.state_id).\
+            order_by(City.id):
+        print("{}: ({}) {}".format(s.name, c.id, c.name))
+    session.close()
