96901f74cd3ff19099420a728133d2e4ab4a8e39
Houcine <113390396+nairbh@users.noreply.github.com>
2024-05-29 09:10:45 +0200

    Update 0-read_file.py

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-"""a function that reads a text file (UTF8) and prints it to stdout:"""
-
-
 def read_file(filename=""):
-    """Read file"""
-    with open(filename, encoding='UTF8') as file:
-        file_read = file.read()
-        print(file_read, end='')
+    """    Reads a text file (UTF8) and prints it to stdout.
+    """
+
+    with open(filename, encoding='utf-8') as f:
+        text = f.read()
+        print(text, end='')
