14fee925ef6e1605f621b7aca032f04130ea98e3
Houcine <113390396+nairbh@users.noreply.github.com>
2024-05-29 09:17:01 +0200

    Update 0-read_file.py

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,16 +1,11 @@
 #!/usr/bin/python3
-    """
-    Reads a text file (UTF8) and prints it to stdout.
+"""
+Module 0-read_file
+A module that contains a function to read and print a text file.
+"""
 
-    Args:
-        filename : The name of the file to be read.
-
-    Returns:
-        None
-    """
 def read_file(filename=""):
     """a function that reads a text file (UTF8) and prints it to stdout:"""
-
     with open(filename, encoding='utf-8') as f:
         text = f.read()
         print(text, end='')
