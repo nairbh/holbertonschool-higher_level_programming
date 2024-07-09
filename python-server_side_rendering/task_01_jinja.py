6e2c43f8bfc4e72a6b7d7f5bfac4348325fb87f2
nairbh <houcinenair@proton.me>
2024-07-09 13:53:10 +0200

    update header, create items.html and items.json

--- python-server_side_rendering/task_01_jinja.py
+++ python-server_side_rendering/task_01_jinja.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
-
+import os
+import json
 app = Flask(__name__)
 
 @app.route('/')
@@ -14,6 +15,13 @@ def about():
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
+
+
+@app.route('/items')
+def items():
+    with open('items.json') as file:
+        _list = json.load(file).get("items", [])
+    return render_template('items.html', items=_list)
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0', port=5000)
