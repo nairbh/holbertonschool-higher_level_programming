36c81768ca074c2bf7ee8b96e08d56edc0f2a891
Houcine <113390396+nairbh@users.noreply.github.com>
2024-07-24 14:35:20 +0200

    Update 0-add_integer.py

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,12 +1,20 @@
 #!/usr/bin/python3
 """define function add rwo integers"""
 
+#!/usr/bin/python3
+"""
+This module is documented
+"""
 
-def add_integer(a, b=98):
-    '''function that adds 2 integers'''
-    if not isinstance(a, (int, float)):
+def add_integer(a, b = 98):
+    """Add two integers
+    >>> add_integer(1, 2)
+    3
+    >>> add_integer(3, 4)
+    7
+    """
+    if (not isinstance(a, (int, float))):
         raise TypeError("a must be an integer")
-    if not isinstance(b, (int, float)):
+    elif (not isinstance(b, (int, float))):
         raise TypeError("b must be an integer")
-
-    return int(a) + int(b)
+    return int(round(a)) + int(round(b))
